342160332d3167619a500f1ed65d691efda21627
Darlysson Nascimento <37389803+Darlysson10@users.noreply.github.com>
2023-05-26 17:16:10 -0300

    Add files via upload

--- youtube.py
+++ youtube.py
@@ -12,20 +12,18 @@ def generate_download_link(url):
         st.error(f"Ocorreu um erro ao gerar o link de download do vídeo: {str(e)}")
         return None
 
-
 def download_video(url, output_path):
     try:
         download_link = generate_download_link(url)
         if download_link:
             st.markdown(f"Download link: [{download_link}]({download_link})")
-            if not os.getenv("STREAMLIT_SERVER_ENDPOINT"):
+            if not os.getenv("STREAMLIT_PRODUCTION"):
                 youtube = YouTube(url)
                 video = youtube.streams.get_highest_resolution()
                 video.download(output_path=output_path)
                 st.success("Download completo.")
     except Exception as e:
         st.error(f"Ocorreu um erro ao baixar o vídeo: {str(e)}")
-
 
 def download_videos_from_file(file_path, output_path):
     try:
@@ -39,13 +37,12 @@ def download_videos_from_file(file_path, output_path):
     except Exception as e:
         st.error(f"Ocorreu um erro ao ler o arquivo: {str(e)}")
 
-
 def main():
     st.title("Download de Vídeos do YouTube")
     st.write("Faça o upload de um arquivo de texto contendo os links dos vídeos do YouTube para iniciar o download.")
 
     file = st.file_uploader("Selecione um arquivo de texto", type=['txt'])
-    if not os.getenv("STREAMLIT_SERVER_ENDPOINT"):
+    if not os.getenv("STREAMLIT_PRODUCTION"):
         output_path = st.text_input("Informe o caminho onde os arquivos serão salvos")
 
     if file is not None and output_path:
@@ -54,7 +51,6 @@ def main():
             f.write(file.getvalue())
 
         download_videos_from_file(file_path, output_path)
-
 
 if __name__ == '__main__':
     main()
