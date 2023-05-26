79923895686dda8497c53773b617435a6f180c1b
Darlysson Nascimento <37389803+Darlysson10@users.noreply.github.com>
2023-05-26 17:09:04 -0300

    Add files via upload

--- youtube.py
+++ youtube.py
@@ -1,15 +1,31 @@
 import streamlit as st
 from pytube import YouTube
-import pytube
+import os
 
-def download_video(url, output_path):
+def generate_download_link(url):
     try:
         youtube = YouTube(url)
         video = youtube.streams.get_highest_resolution()
-        video.download(output_path=output_path)
-        st.success("Download completo.")
+        download_link = video.url
+        return download_link
+    except Exception as e:
+        st.error(f"Ocorreu um erro ao gerar o link de download do vídeo: {str(e)}")
+        return None
+
+
+def download_video(url, output_path):
+    try:
+        download_link = generate_download_link(url)
+        if download_link:
+            st.markdown(f"Download link: [{download_link}]({download_link})")
+            if not os.getenv("STREAMLIT_SERVER_ENDPOINT"):
+                youtube = YouTube(url)
+                video = youtube.streams.get_highest_resolution()
+                video.download(output_path=output_path)
+                st.success("Download completo.")
     except Exception as e:
         st.error(f"Ocorreu um erro ao baixar o vídeo: {str(e)}")
+
 
 def download_videos_from_file(file_path, output_path):
     try:
@@ -23,11 +39,14 @@ def download_videos_from_file(file_path, output_path):
     except Exception as e:
         st.error(f"Ocorreu um erro ao ler o arquivo: {str(e)}")
 
+
 def main():
     st.title("Download de Vídeos do YouTube")
     st.write("Faça o upload de um arquivo de texto contendo os links dos vídeos do YouTube para iniciar o download.")
+
     file = st.file_uploader("Selecione um arquivo de texto", type=['txt'])
-    output_path = st.text_input("Informe o caminho onde os arquivos serão salvos")
+    if not os.getenv("STREAMLIT_SERVER_ENDPOINT"):
+        output_path = st.text_input("Informe o caminho onde os arquivos serão salvos")
 
     if file is not None and output_path:
         file_path = file.name
@@ -35,6 +54,7 @@ def main():
             f.write(file.getvalue())
 
         download_videos_from_file(file_path, output_path)
+
 
 if __name__ == '__main__':
     main()
