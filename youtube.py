48031797e9eb15457c0766cc4e5ff30e1a4df4c1
Darlysson <don@ic.ufal.br>
2024-07-23 09:52:50 -0300

    Added mp3 feature

--- youtube.py
+++ youtube.py
@@ -11,7 +11,15 @@ def generate_download_link(url):
     except Exception as e:
         st.error(f"An error occurred while generating the video download link: {str(e)}")
         return None
-
+def generate_mp3_download_link(url):
+    try:
+        youtube = YouTube(url)
+        video = youtube.streams.get_audio_only()
+        download_link = video.url
+        return download_link
+    except Exception as e:
+        st.error(f"An error occurred while generating the video download link: {str(e)}")
+        return None
 def download_video(url, output_path):
     try:            
         youtube = YouTube(url)
@@ -52,9 +60,25 @@ def download_video_from_links(urls, output_path, is_local):
     except Exception as e:
         st.error(f"An error occurred while downloading the video: {str(e)}")
 
+def download_mp3_from_links(urls, output_path, is_local):
+    try:
+        total_videos = len(urls)
+        for i, url in enumerate(urls, start=1):
+            st.write(f"Downloading video {i} of {total_videos}")
+            if is_local and output_path:
+                download_video(url, output_path)
+            else:
+                download_link = generate_mp3_download_link(url)
+                if download_link:
+                    st.write(f"Download link: {download_link}")
+    except Exception as e:
+        st.error(f"An error occurred while downloading the video: {str(e)}")
+
+
 def main():
     st.title("YouTube Video Downloader")
     is_local = st.checkbox("I'm running locally.")
+    mp3 = st.checkbox("Download as MP3")
     # st.write("Upload a text file containing the links (separated by lines) of YouTube videos to initiate the download.")
     st.write("Enter the links manually on the text box below.")
     urls = st.text_area("Enter the links below (separated by lines):")
@@ -71,8 +95,11 @@ def main():
     #         f.write(file.getvalue())
 
     #     download_videos_from_file(file_path, output_path, is_local)
-    elif urls:
+    elif urls and not mp3:
         download_video_from_links(urls, output_path, is_local)
+    elif urls and mp3:
+        download_mp3_from_links(urls, output_path, is_local)
+    
     
 
 if __name__ == '__main__':
